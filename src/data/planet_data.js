7dbd3c1bb27a6f987a10980b031b107535f026f2
Devin Cooley <dcooley1350@gmail.com>
2021-05-21 22:07:12 -0700

    Add data directory and planet data

---
+++ src/data/planet_data.js
@@ -0,0 +1,48 @@
+
+export default Planets = {
+  mercury: {
+    Name: "Mercury",
+    hasBirds: false,
+    justification: "it just doesn't",
+  },
+  venus: {
+    Name: "Venus",
+    hasBirds: false,
+    justification: "it just doesn't",
+  },
+  earth: {
+    Name: "Earth",
+    hasBirds: true,
+    justification: "It does! Can confirm",
+  },
+  mars: {
+    Name: "Mars",
+    hasBirds: false,
+    justification: "it just doesn't",
+  },
+  jupiter: {
+    Name: "Jupiter",
+    hasBirds: false,
+    justification: "it just doesn't",
+  },
+  saturn: {
+    Name: "Saturn",
+    hasBirds: false,
+    justification: "it just doesn't",
+  },
+  uranus: {
+    Name: "Uranus",
+    hasBirds: false,
+    justification: "it just doesn't",
+  },
+  neptune: {
+    Name: "neptune",
+    hasBirds: false,
+    justification: "it just doesn't",
+  },
+  pluto: {
+    Name: "Pluto",
+    hasBirds: false,
+    justification: "it just doesn't",
+  },
+}
